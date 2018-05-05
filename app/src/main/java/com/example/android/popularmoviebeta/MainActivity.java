babfa47dd925a732c53c63b5997609b25d1781d6
Diaz Agasatya <diazagasatya@gmail.com>
2018-05-05 16:49:14 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/popularmoviebeta/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.popularmoviebeta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.popularmoviebeta.Utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        URL url = NetworkUtils.buildURL();
+        
+    }
+}
---
+++ app/src/main/java/com/example/android/popularmoviebeta/Utilities/NetworkUtils.java
@@ -0,0 +1,101 @@
+package com.example.android.popularmoviebeta.Utilities;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by diazagasatya on 5/5/18.
+ */
+public class NetworkUtils {
+
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+
+    // Please insert your MovieDB API KEY here before running
+    // This API Key can be obtained by registering to https://developers.themoviedb.org
+    private static final String API_KEY = "546ac5863d9083b1cf9d85daabc5446a";
+
+    /* API URL FORMATTING */
+    private static final String VERSION = "3"; // Current API version available
+    private static final String MOVIE = "movie"; // Movie path
+    private static final String NOW_PLAYING = "now_playing"; // Shows now playing
+    private static final String LANGUAGE_PARAM = "language";
+    private static final String LANGUAGE = "en-US"; // Show in English
+    private static final String PAGE_PARAM = "page";
+    private static final String PAGE_NUMBER = "1"; // Number of page shows
+    private static final String API_KEY_PARAM = "api_key";
+
+
+    // Base URL for movieDB API
+    private static final String BASE_URL_MOVIE = "https://api.themoviedb.org";
+
+    /**
+     * Build the URL needed to grab JSON data from movieDB API.
+     * @return the newly built URL
+     */
+    public static URL buildURL() {
+
+        // Build URI for http request
+        Uri uriBuilt = Uri.parse(BASE_URL_MOVIE).buildUpon()
+                .appendPath(VERSION)
+                .appendPath(MOVIE)
+                .appendPath(NOW_PLAYING)
+                .appendQueryParameter(API_KEY_PARAM, API_KEY)
+                .appendQueryParameter(LANGUAGE_PARAM, LANGUAGE)
+                .appendQueryParameter(PAGE_PARAM,PAGE_NUMBER)
+                .build();
+
+        // Build URL from URI
+        URL url = null;
+
+        try {
+            url = new URL(uriBuilt.toString());
+
+            // Print the Url for debugging purposes
+            Log.v(TAG, url.toString());
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    /**
+     * This function will get response from HTTP URL & return the entire result
+     * @param url Http URL after build URI
+     * @return the entire result of http as a String
+     * @throws IOException
+     */
+    public static String getResponseFromHttpURL(URL url) throws IOException {
+
+        // open connection using the url passed
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        // Grab all of the string values from http connection
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+
+            boolean hasInputs = scanner.hasNext();
+
+            if(hasInputs) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+
+            // Disconnect from the connection
+            urlConnection.disconnect();
+        }
+    }
+
+}
