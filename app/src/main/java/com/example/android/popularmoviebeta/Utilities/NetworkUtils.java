3f266acaf9fc2c0f7cab2e04ddfcc003f12e154c
Diaz Agasatya <diazagasatya@gmail.com>
2018-05-16 11:50:45 -0700

    Adapter and Recycler View Updated

--- app/src/main/java/com/example/android/popularmoviebeta/Utilities/NetworkUtils.java
+++ app/src/main/java/com/example/android/popularmoviebeta/Utilities/NetworkUtils.java
@@ -2,6 +2,7 @@ package com.example.android.popularmoviebeta.Utilities;
 
 import android.net.Uri;
 import android.util.Log;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
